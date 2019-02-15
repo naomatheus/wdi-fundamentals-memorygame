902d5e82b63b111cc41c5e5e8fb51596ed178019
matthew Robinson <matthewrobinson@matthews-air.attlocal.net>
2019-02-14 20:38:25 -0600

    added functional game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,66 +1,46 @@
 console.log("Up and running");
 
-// var cardTwo = "king";
-// var cardThree = "queen";
-// var cardFour = "queen";
-
 
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  console.log("cardsInPlay length is " + cardsInPlay.length);
+var checkForMatch = function(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You have a match!");
   } else if (cardsInPlay[0] !== cardsInPlay[1]) {
     alert("Sorry, try again");
   }
-}
+};
 
+var flipCard = function(cardId){
+  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId]);
+  if (cardsInPlay.length === 2) {
+  }
+};
 
+flipCard(0);
+flipCard(2);
+checkForMatch();
 
-// Inside of the if statement you created in the last step, create an if...else statement that checks for equality between the two cards in the cardsInPlay array.
-// For the condition, check to see if the first card in the cardsInPlay array (cardsInPlay[0]) is equal to the second card in the cardsInPlay array.
-
-
-// Write an if statement that checks to see if the length of the cardsInPlay array is 2.
-// For the condition, you can use the length property to find out how many items are in the cardsInPlay array and then use the === operator to see if the length is equal to 2.
-
-
-// 3rd step
-// Create a variable cardTwo.
-// Store the third item in the cards array in the variable cardTwo.
-// Now we'll want to add this second card to the cardsInPlay array.
+// Next, we'll want to add the card that the user flipped to the cardsInPlay array.
 //
-// Use the push() method to add cardTwo to the end of the cardsInPlay array.
+// Use the push method to add the card that the user flipped (cards[cardId]) to the cardsInPlay array.
+// Now let's call the flipCard function to simulate the user flipping a card.
+//
+// On the line after the flipCard function, call the flipCard function, passing in 0 as an argument.
 
-// 2nd step
-// Next up, you'll create a variable to represent the first card that the user flipped.
-// Create a variable cardOne.
-// Store the first item in the cards array in the variable cardOne.
-// HINT: Use the array name, square brackets, and the index number to retrieve this card from the array.
+// Within the flipCard function, add a console.log() statement to display the card that was flipped over.
+// We'll use the index number that is passed in when the flipCard function is called to access this card from the cards array like so: cards[cardId].
+// The message that is logged in the console should read "User flipped queen" or "User flipped king".
+
+
+// In your main.js file, after the lines with the cards and cardsInPlay arrays, create a function flipCard.
+// The function should accept one parameter: cardId.
+// Move all the code that you wrote for the last unit's assignment, except for the cards and cardsInPlay arrays into the flipCard function.
+// NOTE: We are leaving the two arrays outside of the flipCard function since we want to make sure that the two arrays have global scope.
+
+
+// First you'll create a function that will store all steps related to selecting, or flipping over, a card. When the user flips a card over, you'll want to add that card to the array of cards that are in play. If the user has flipped over two cards, you'll want to check for a match.
 //
-// Now we'll want to add this first card to the cardsInPlay array.
-// Use the push() method to add cardOne to the end of the cardsInPlay array.
-// Here's an example of the push() method if you need a syntax refresher:
-// ghosts.push('clyde');
-// 1st step
-// In your main.js file, create an array to store the four cards.
-//
-// Create an array cards.
-// The array should hold four strings - one for each of our cards.
-// The strings should be "queen", "queen", "king", and "king".
-// After creating the cards array, you can delete the variables you created in the last unit's project.
-// Next we'll want to create an empty array. This will eventually hold the cards that are in play.
-//
-// Create an array cardsInPlay.
-// Set its value to an empty array []
+// Second, you'll create a function to group together the logic to check to see if the two cards that the user has flipped over match each other. You'll provide feedback to the user letting them know if the two cards match, or if they should try again.
